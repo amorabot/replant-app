ce51a6f77619b8c8c3941e84f1ae2b2e4a0cb20e
Daniel Amorim <danielamorimbessa@hotmail.com>
2023-07-10 12:46:54 -0300

    User(deprecated) functionalities commented out
    Commented user entity functionalities for later reference. It was simply a study subject and a placeholder.

--- replantSpring/src/main/java/br/com/replantapp/replant/domain/userDEPRECATED/User.java
+++ replantSpring/src/main/java/br/com/replantapp/replant/domain/userDEPRECATED/User.java
@@ -1,30 +1,30 @@
-package br.com.replantapp.replant.domain.userDEPRECATED;
-
-import jakarta.persistence.*;
-import lombok.*;
-
-@Table(name = "users")
-@Entity(name = "users")
-
-@NoArgsConstructor
-@AllArgsConstructor
-@Getter
-@Setter
-@EqualsAndHashCode(of = "id")
-public class User {
-
-    @Id @GeneratedValue(strategy = GenerationType.UUID)
-    private String id;
-    private String name;
-    private String region;
-    private String url;
-
-    public User(UserRequestDTO userData){//criar um usuário com dados vindo do request do cliente
-        // não precisamos criar o ID, é automático
-        name = userData.name();
-        region = userData.region();
-        url = userData.url();
-    }
-}
-
-
+//package br.com.replantapp.replant.domain.userDEPRECATED;
+//
+//import jakarta.persistence.*;
+//import lombok.*;
+//
+//@Table(name = "users")
+//@Entity(name = "users")
+//
+//@NoArgsConstructor
+//@AllArgsConstructor
+//@Getter
+//@Setter
+//@EqualsAndHashCode(of = "id")
+//public class User {
+//
+//    @Id @GeneratedValue(strategy = GenerationType.UUID)
+//    private String id;
+//    private String name;
+//    private String region;
+//    private String url;
+//
+//    public User(UserRequestDTO userData){//criar um usuário com dados vindo do request do cliente
+//        // não precisamos criar o ID, é automático
+//        name = userData.name();
+//        region = userData.region();
+//        url = userData.url();
+//    }
+//}
+//
+//
--- replantSpring/src/main/java/br/com/replantapp/replant/domain/userDEPRECATED/UserResponseDTO.java
+++ replantSpring/src/main/java/br/com/replantapp/replant/domain/userDEPRECATED/UserResponseDTO.java
@@ -1,9 +1,9 @@
-package br.com.replantapp.replant.domain.userDEPRECATED;
-
-// DTO - Data Transfer Object
-// Serve para não trabalharmos diretamente com as entidades que representam a tabela User e usar objetos mais diretos e simples no lugar.
-//public record UserResponseDTO(Long id, String name, String region, String url) {
-//    public UserResponseDTO(User user){
-//        this(user.getId(), user.getName(), user.getRegion(), user.getUrl());
-//    }
-//}
+//package br.com.replantapp.replant.domain.userDEPRECATED;
+//
+//// DTO - Data Transfer Object
+//// Serve para não trabalharmos diretamente com as entidades que representam a tabela User e usar objetos mais diretos e simples no lugar.
+////public record UserResponseDTO(Long id, String name, String region, String url) {
+////    public UserResponseDTO(User user){
+////        this(user.getId(), user.getName(), user.getRegion(), user.getUrl());
+////    }
+////}
